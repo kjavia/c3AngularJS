16528f5aece1c3cd92f159ced88d23405f08b6ac
Ketan Javia <k_javia@hotmail.com>
2015-04-17 22:28:33 -0500

    added missing scripts folder

---
+++ scripts/src/app.demo.js
@@ -0,0 +1,62 @@
+(function() {
+  'use strict';
+  angular.module('demoApp', ['ui.router', 'c3-charts'])
+    .config(function($stateProvider, $urlRouterProvider) {
+      $stateProvider
+        .state('example1', {
+          url: '/example1',
+          templateUrl: 'example1.html',
+          data: {
+            title: 'Example 1 - Basic'
+          }
+        })
+        .state('example2', {
+          url: '/example2',
+          templateUrl: 'example2.html',
+          data: {
+            title: 'Example 2 - Mouse Events'
+          }
+        })
+        .state('example3', {
+          url: '/example3',
+          templateUrl: 'example3.html',
+          data: {
+            title: 'Example 3 - Chart Type'
+          }
+        })
+        .state('example4', {
+          url: '/example4',
+          templateUrl: 'example4.html',
+          data: {
+            title: 'Example 4 - Multiple Groups'
+          }
+        });
+      $urlRouterProvider.otherwise("/example1");
+    })
+    .directive('prismCode', [
+      '$timeout',
+      function($timeout) {
+        return {
+          restrict: 'A',
+          priority: 0,
+          compile: function(tElement, tAttributes, transclude) {
+            return {
+              post: function(scope, element, attributes) {
+                var $element = $(element);
+                $timeout(function() {
+                  Prism.fileHighlight();
+                }, 0);
+              }
+            }
+          }
+        }
+      }
+    ])
+    .run(function($rootScope, $state) {
+      $rootScope.$state = $state;
+      $rootScope.allStates = $.grep($state.get(), function(s) {
+        return s.name.length > 0;
+      });;
+    });
+
+})();
---
+++ scripts/src/example3.controller.js
@@ -0,0 +1,33 @@
+(function() {
+  'use strict';
+  angular.module('demoApp')
+    .controller('Example3Controller', example3Ctrl);
+
+  example3Ctrl.$inject = ['$scope', '$timeout'];
+
+  function example3Ctrl($scope, $timeout) {
+    var vm = this;
+    vm.group = 'group3';
+    vm.elementId = 'example3';
+
+    vm.data = {
+      bindto: '#' + this.elementId,
+      data: {
+        columns: [
+          ['data1', 30],
+          ['data2', 120],
+        ],
+        type: 'donut'
+      },
+      donut: {
+        title: "Iris Petal Width"
+      }
+    };
+    $timeout(function() {
+      //give it a sec for the directive to be attached.
+      $scope.$broadcast('c3.generate', {
+        group: vm.group
+      });
+    });
+  }
+})();
